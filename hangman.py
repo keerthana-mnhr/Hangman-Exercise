306d7147ffb1cb9c32e0fab385a134cfc1b10fdb
Keerthana Manoharan <keerthana.mnhr@gmail.com/Users/keerthanamanoharan/Desktop/FromClass/Repos/Hangman-Exercise/venv/bin/python>
2025-04-21 00:09:58 +0200

    fixed lowe cases and messsages

--- hangman.py
+++ hangman.py
@@ -11,7 +11,7 @@ predefined_hangman_words = [
 
 def hangman():
     selected_word = random.choice(predefined_hangman_words)
-    print(selected_word)
+    
     word_length = len(selected_word)
     number_of_remaining_attempts= word_length 
     print( f'Guess the letters in the word!!! Its a {word_length} letter word'  )
@@ -20,8 +20,10 @@ def hangman():
     user_guessed_letters = list(hyphen * word_length)
     selected_word_to_letters = list(selected_word)
     while number_of_remaining_attempts > 0:
-        user_input = input('Enter a letter: ')
-        if user_input.isalpha():
+        user_input = input('Enter a letter: ').lower()
+        if not user_input.isalpha() or len(user_input) !=1:
+            print('Invalid character !! Only one alphabetical character is allowed. Invalid inputs are not taken as an attempt. Try with a valid character'  )
+        else:
             number_of_remaining_attempts -=1
             if user_input in selected_word_to_letters:
                 for l in range(word_length):
@@ -29,8 +31,8 @@ def hangman():
                         user_guessed_letters[l]= user_input                      
             else:
                          print('Wrong letter!! The letter doesnt exists in the word')
-        else:
-            print('Invalid character !! Only alphabetical choise is allowed') 
+        
+             
 
         print(f'Guessed Word: {' '.join(user_guessed_letters)}')  
 
@@ -39,7 +41,7 @@ def hangman():
            break
     else:
          print('Game Over you Lost, Number of attempt to guess the word was over')
-    continue_playing = input('DO you want to continue playing ? (y/n)') 
+    continue_playing = input('Do you want to continue playing ? \n(y/n): ').lower() 
     if continue_playing == 'y':
         hangman()      
     else:
